306419a8469a7a826c1ea25b699dff66a452b3c5
Shulun_titech <112923616+SHUlun0214@users.noreply.github.com>
2022-09-08 16:10:11 +0900

    Update preprocess python code.py

--- preprocess python code.py
+++ preprocess python code.py
@@ -1,7 +1,9 @@
+from re import I
 import pyxdf
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
+import math
 from scipy import signal
 from pathlib import Path
 #-------------------------- load the data --------------------------
@@ -10,19 +12,18 @@ data, header = pyxdf.load_xdf(data_file)
 data1 = data[0]    # read the trigger signal data
 data2 = data[1]    # read the raw EMG signal data
 sfreq = 1000       # frequency of the sample
-
 #-------------------------- plot the trigger signal --------------------------
 trigger_t = data1['time_stamps']
 trigger_v = data1['time_series']
 trigger_t -= trigger_t[0]            # initialize the time to 0s series
-plt.rcParams['savefig.dpi'] = 300    #图片像素       200=1200*800
-#plt.rcParams['figure.dpi'] = 300    #图片分辨率     300=1800*1200
+plt.rcParams['savefig.dpi'] = 300    # pixel of image       200=1200*800
+#plt.rcParams['figure.dpi'] = 300    # resolution of image     300=1800*1200
 plt.figure(figsize=(20,5))
+plt.subplot(2,2,1)
 plt.title('Trigger signal', fontsize=10)
-plt.plot(trigger_t, trigger_v, linewidth=0.5, label='trigger signal')
+plt.plot(trigger_t, trigger_v, drawstyle='steps-post', linewidth=0.5, label='trigger signal')
 plt.xlabel('t(s)' , fontsize=10)
 plt.ylabel('amplitude(μV)', fontsize=10)
-plt.show()
 #-------------------------- plot the raw EMG signal --------------------------
 emg_t = data2['time_stamps']
 emg_v = data2['time_series']   # EMG signal has 16 channels
@@ -31,13 +32,11 @@ for i in emg_v:
     emg.append(i[0])           # the first channel is the signal data of masseter
 emg_t = emg_t - emg_t[0]       # initialize the time to 0s series
 emg_v = emg
-plt.rcParams['savefig.dpi'] = 300   #图片像素       200=1200*800
-plt.figure(figsize=(20,5))
+plt.subplot(2,2,2)
 plt.title('Original Signal', fontsize=10)
 plt.plot(emg_t, emg_v, linewidth=0.5, label='raw signal')
 plt.xlabel('t(s)' , fontsize=10)
 plt.ylabel('amplitude(μV)', fontsize=10)
-plt.show()
 #-------------------------- preprocess the EMG signal --------------------------
 # process EMG signal: remove mean
 emg_v = emg_v - np.mean(emg_v)
@@ -57,11 +56,48 @@ b2, a2 = scipy.signal.butter(4, low_pass, btype='lowpass')
 emg_filtered = abs(scipy.signal.filtfilt(b, a, emg_v))
 # Apply to rectified signal to get EMG envelope
 emg_filtered = scipy.signal.filtfilt(b2, a2, emg_filtered)
-plt.rcParams['savefig.dpi'] = 300   #图片像素       200=1200*800
-plt.figure(figsize=(20,5))
+plt.subplot(2,2,3)
 plt.title('Filtered signal', fontsize=10)
 plt.plot(emg_t, emg_v, linewidth=0.5, color = 'orange', label='raw signal')
 plt.plot(emg_t, emg_filtered, linewidth=0.5,color = 'blue', label='filtered signal')
+plt.xlabel('t(s)' , fontsize=10)
+plt.ylabel('amplitude(μV)', fontsize=10)
+#-------------------------- cut the contraction process --------------------------
+# define a funtion to find the contraction process
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return idx
+# cut the contraction process
+contract_t_idx = []
+contract_t = []
+contract_emg = []
+for i in [4, 7, 10, 13]:
+    t_s = trigger_t[i]
+    t_e = trigger_t[i + 1]
+    a = find_nearest(emg_t, t_s)
+    b = find_nearest(emg_t, t_e)
+    contract_t.append(emg_t[a: b])
+    contract_emg.append(emg_filtered[a: b])
+    contract_t_idx.append(a)
+    contract_t_idx.append(b)
+plt.rcParams['savefig.dpi'] = 300   # pixel of image       200=1200*80
+plt.figure(figsize=(20,5))
+plt.title('contraction signal', fontsize=10)
+plt.subplot(2,2,1)
+plt.plot(contract_t[0], contract_emg[0], linewidth=0.5, color = 'orange', label = 'contraction 1')
+plt.xlabel('t(s)' , fontsize=10)
+plt.ylabel('amplitude(μV)', fontsize=10)
+plt.subplot(2,2,2)
+plt.plot(contract_t[1], contract_emg[1], linewidth=0.5, color = 'orange', label = 'contraction 2')
+plt.xlabel('t(s)' , fontsize=10)
+plt.ylabel('amplitude(μV)', fontsize=10)
+plt.subplot(2,2,3)
+plt.plot(contract_t[2], contract_emg[2], linewidth=0.5, color = 'orange', label = 'contraction 3')
+plt.xlabel('t(s)' , fontsize=10)
+plt.ylabel('amplitude(μV)', fontsize=10)
+plt.subplot(2,2,4)
+plt.plot(contract_t[3], contract_emg[3], linewidth=0.5, color = 'orange', label = 'contraction 4')
 plt.xlabel('t(s)' , fontsize=10)
 plt.ylabel('amplitude(μV)', fontsize=10)
 plt.show()
